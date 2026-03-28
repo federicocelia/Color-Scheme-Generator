2dd73b53d1a623d4f44f78f5b24cd24a0583937e
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2026-03-28 01:57:35 +0000

    Basic working version: index.js now fetches color schemes from The Color API and renders background colors and hex names in the UI

---
+++ index.js
@@ -0,0 +1,38 @@
+const pickedColor = document.querySelector("#color-input");
+const colorTheory = document.querySelector("select");
+const numberOfColors = 5;
+const renderingColors = [];
+
+document.querySelector("#get-color-scheme").addEventListener("click", () => {
+  const hexCol = pickedColor.value.slice(1);
+  const colorTheoryValue = colorTheory.value;
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${hexCol}&mode=${colorTheoryValue}&count=${numberOfColors}&format=json`,
+
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    },
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      data.colors.forEach((color) => renderingColors.push(color.hex.value));
+      console.log(data.colors[0].hex.value);
+      console.log(data.colors[1].hex.value);
+      console.log(data.colors[2].hex.value);
+      console.log(data.colors[3].hex.value);
+      console.log(data.colors[4].hex.value);
+      console.log(renderingColors);
+
+      const [color0, color1, color2, color3, color4] = renderingColors;
+      renderingColors.forEach((color, index) => {
+        document.querySelector(`.color${index}`).style.backgroundColor = color;
+        document.querySelector(`.name${index}`).textContent = color;
+      });
+
+      renderingColors.length = 0;
+    });
+});
